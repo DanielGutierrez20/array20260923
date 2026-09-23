f3582baeac182bd30091bad2212c91d491d6a0ad
DanielGutierrez20 <n00507628@upn.pe>
2026-09-23 12:36:45 -0500

    Impresion de notas

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+static void ImprimirArreglo(int[] notas)
+{
+    for (int i = 0; i < notas.Length; i++)
+    {
+        Console.WriteLine($"Nota {i+1}: {notas[i]}");
+    }
+}
+
+static void SumarArreglo(int[] notas)
+{
+    int suma = notas.Sum();
+    Console.WriteLine($"Suma de las notas: {suma}");
+}
+
+int[] notas = {15, 18, 12, 17, 20};
+ImprimirArreglo(notas);
+SumarArreglo(notas);
+
+//int[] notas2 = {2, 4, 9};
+//ImprimirArreglo(notas2);
